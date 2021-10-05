412cbc3b2d0564acf5fe6ac33bc1debd42f3db0b
Yudi Candra <yudhichandra02@gmail.com>
2021-10-05 07:04:04 +0700

    first commit

---
+++ app/Http/Livewire/PrefviewPdfController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PrefviewPdfController extends Component
+{
+  public $pdf;
+
+  public function mount($file = null)
+  {
+    dd($file);
+    if (!$file) return abort(404);
+
+    return response()->file(storage_path('upload/' . $file));
+  }
+}
