155a50bbd973388fa9b4cf1ca114c4d76dadb687
Yudi Candra <yudhichandra02@gmail.com>
2021-10-05 08:24:54 +0700

    update menu arsip 9

--- resources/views/livewire/tbl-archives.blade.php
+++ resources/views/livewire/tbl-archives.blade.php
@@ -52,9 +52,14 @@
                             label="File Arsip" />
                     </div>
                     <div class="modal-footer">
-                        <button type="button" wire:click={{$update_mode ? 'update('.$jenis_arsip_id.')'
-                            : 'store('.$jenis_arsip_id.')' }} class="btn btn-primary btn-sm"><i
+                        @if ($update_mode)
+                        <button type="button" wire:click='update({{$jenis_arsip_id}})' class="btn btn-primary btn-sm"><i
                                 class="fa fa-check pr-2"></i>Simpan</button>
+                        @else
+                        <button type="button" wire:click='store({{$jenis_arsip_id}})' class="btn btn-primary btn-sm"><i
+                                class="fa fa-check pr-2"></i>Simpan</button>
+                        @endif
+
                         <button class="btn btn-danger btn-sm" wire:click='_reset'><i
                                 class="fa fa-times pr-2"></i>Batal</a>
                     </div>
